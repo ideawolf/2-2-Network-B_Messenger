44b49821c8474eb5addc99bab340bd7b2e8e6ac6
BSY <bsy@email.com>
2022-11-13 19:37:43 +0900

    d

--- src/Server.java
+++ src/Server.java
@@ -1,12 +1,31 @@
 import java.net.*;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.concurrent.*;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         ServerSocket listener = new ServerSocket(35014);
 
         ExecutorService pool = Executors.newFixedThreadPool(20);
+
+        Connection con = DriverManager.getConnection("jdbc:sqlite:/Users/ideawolf/Documents/GitHub/B_Messenger/db.sqlite3");
+
+
+        // PostID is Auto_increment.
+        String query = "INSERT INTO user (user_id, password, nickname, email)\n" +
+                "VALUES ( ?, ?, ?, ?);";
+
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setString(1, "userId1");
+        ps.setString(2, "userPassword1");
+        ps.setString(3, "Nickname1");
+        ps.setString(4, "user1@email.com");
+
+        ps.executeUpdate();
 
         while (true) {
             Socket socket = listener.accept();
@@ -23,7 +42,6 @@ public class Server {
 
         @Override
         public void run() {
-
 
         }
 
