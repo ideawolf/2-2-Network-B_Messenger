b223729125ed2376d8cb63160b180db131d90ac3
BSY <ideawolf@protonmail.com>
2022-12-09 16:05:32 +0900

    design protocol

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
 import javax.swing.*;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import org.json.JSONObject;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        JSONObject json = new JSONObject();
+        json.put("command", "REGISTER");
+        json.put("name", "test_name");
+        json.put("password", "test_password");
+        json.put("nickname", "test_nickname");
+        json.put("email", "test_email@email.com");
+        Socket s = new Socket("localhost", 35014);
+        try (OutputStreamWriter out = new OutputStreamWriter(
+                s.getOutputStream(), StandardCharsets.UTF_8)) {
+            out.write(json.toString());
+        }
 
         new Login();
     }
--- src/Server.java
+++ src/Server.java
@@ -1,31 +1,15 @@
 import java.net.*;
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Scanner;
 import java.util.concurrent.*;
+import org.json.JSONObject;
 
 public class Server {
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(35014);
 
         ExecutorService pool = Executors.newFixedThreadPool(20);
-
-        Connection con = DriverManager.getConnection("jdbc:sqlite:db.sqlite3");
-
-
-        // PostID is Auto_increment.
-        String query = "INSERT INTO user (user_id, password, nickname, email)\n" +
-                "VALUES ( ?, ?, ?, ?);";
-
-        PreparedStatement ps = con.prepareStatement(query);
-        ps.setString(1, "userId1");
-        ps.setString(2, "userPassword1");
-        ps.setString(3, "Nickname1");
-        ps.setString(4, "user1@email.com");
-
-        ps.executeUpdate();
 
         while (true) {
             Socket socket = listener.accept();
@@ -42,7 +26,52 @@ public class Server {
 
         @Override
         public void run() {
+            System.out.println("Connected: " + socket);
 
+            try {
+                var in = new Scanner(socket.getInputStream());
+
+                while (in.hasNextLine()) {
+                    String input = in.nextLine();
+                    System.out.println(socket.getInetAddress().toString() + " 클라이언트가 전송함 : " + input);
+
+                    JSONObject recieve_json = new JSONObject(input);
+
+                    boolean status = false;
+
+                    if(recieve_json.getString("command").equals("REGISTER")){
+                        status = register(recieve_json);
+                    }
+
+                    System.out.println("result: " + status);
+
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+
+        public boolean register(JSONObject input) throws SQLException {
+            Connection con = DriverManager.getConnection("jdbc:sqlite:db.sqlite3");
+            // PostID is Auto_increment.
+            String query = "INSERT INTO user (user_id, password, nickname, email)\n" +
+                    "VALUES ( ?, ?, ?, ?);";
+
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, input.getString("name"));
+            ps.setString(2, input.getString("password"));
+            ps.setString(3, input.getString("nickname"));
+            ps.setString(4, input.getString("email"));
+
+            int updateResult = ps.executeUpdate();
+
+            if(updateResult > 0){
+                return true;
+            }
+
+            return false;
         }
 
 
