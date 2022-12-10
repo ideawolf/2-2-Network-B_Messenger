073d5f7dfc75e4e0b9248505c72b1d060464ea87
Dohun <kdhhuns2000@gmail.com>
2022-12-10 23:44:27 +0900

    Add File IO

--- src/ChatRoom.java
+++ src/ChatRoom.java
@@ -1,9 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
+import java.util.*;
 
 public class ChatRoom extends Frame {
     public static void main(String[] args) {
@@ -12,17 +10,19 @@ public class ChatRoom extends Frame {
 
     ImageIcon file_upload_img = new ImageIcon("images/file_upload.png");
 
+    private final JFileChooser fileChooser = new JFileChooser();
+
     ChatRoom() {
         setSize(600, 600);
         setLayout(new FlowLayout());
 
-        TextArea chatList = new TextArea("하고 싶은 말을 적으세요.", 30, 80, TextArea.SCROLLBARS_VERTICAL_ONLY);
+        TextArea chatList = new TextArea("", 30, 80, TextArea.SCROLLBARS_VERTICAL_ONLY);
         chatList.setEditable(false);
 
         JButton fileSendButton = new JButton(file_upload_img);
         fileSendButton.setPreferredSize(new Dimension(40, 40));
 
-        TextArea sendArea = new TextArea("적을 내용", 4, 60, TextArea.SCROLLBARS_NONE);
+        TextArea sendArea = new TextArea("", 4, 60, TextArea.SCROLLBARS_NONE);
 
         JButton sendButton = new JButton("보내기");
         sendButton.setPreferredSize(new Dimension(80, 80));
@@ -43,12 +43,20 @@ public class ChatRoom extends Frame {
             }
         });
 
-        fileSendButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
+        fileSendButton.addActionListener(e -> {
+            fileChooser.showOpenDialog(this);
+            StringTokenizer tokenizer = new StringTokenizer(fileChooser.getSelectedFile().toString(), "\\");
+            ArrayList<String> splitStr = new ArrayList<>();
+            while(tokenizer.hasMoreTokens()) {
+                splitStr.add(tokenizer.nextToken());
+            }
+            int option = JOptionPane.showOptionDialog(null, splitStr.get(splitStr.size() - 1) + " 파일을 전송하시겠습니까?", "알림", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[]{"보내기", "취소"}, "보내기");
+            if (option == 0) {
+                chatList.append("유저가 파일을 전송하려고 합니다\n");
+                //FileIO.FileSend(fileChooser.getSelectedFile());
             }
         });
+
 
         setVisible(true);
 
