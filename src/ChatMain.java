f2b98508b43abbfb4b10d41c2352f8f0f0712825
Dohun <kdhhuns2000@gmail.com>
2022-12-11 04:56:05 +0900

    Add weather public data

--- src/ChatMain.java
+++ src/ChatMain.java
@@ -1,18 +1,28 @@
 import java.awt.event.*;
 import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 import javax.swing.*;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
+import javax.swing.border.*;
 import javax.swing.event.*;
+import javax.swing.table.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
 public class ChatMain extends JFrame {
+
 
     public static void main(String[] args) {
         new ChatMain();
     }
 
     ChatMain() {
-        setSize(800,600);
+        setSize(800, 600);
         setResizable(false);
         setLocationRelativeTo(null);
         setLayout(null);
@@ -30,7 +40,7 @@ public class ChatMain extends JFrame {
         // 유저 이름 집어넣어야됨
         JLabel usernameLabel = new JLabel(username);
         usernameLabel.setFont(new Font("맑은 고딕", Font.BOLD, 12));
-        usernameLabel.setBounds(0,25,200,30);
+        usernameLabel.setBounds(0, 25, 200, 30);
         usernameLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(usernameLabel);
 
@@ -42,12 +52,12 @@ public class ChatMain extends JFrame {
 
         JLabel userDailyWordLabel = new JLabel(dailyWordLabel);
         userDailyWordLabel.setFont(new Font("맑은 고딕", Font.BOLD, 12));
-        userDailyWordLabel.setBounds(0,75,200,30);
+        userDailyWordLabel.setBounds(0, 75, 200, 30);
         userDailyWordLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(userDailyWordLabel);
 
         JButton userInfoEdit = new JButton("정보 변경");
-        userInfoEdit.setBounds(250,50,100,30);
+        userInfoEdit.setBounds(250, 50, 100, 30);
         userInfoEdit.setFont(new Font("맑은 고딕", Font.BOLD, 12));
         userInfoEdit.setHorizontalAlignment(JLabel.CENTER);
         userInfoEdit.setForeground(Color.BLACK);
@@ -62,7 +72,7 @@ public class ChatMain extends JFrame {
 
         JLabel friendListDesc = new JLabel("친구목록");
         friendListDesc.setFont(new Font("맑은 고딕", Font.BOLD, 12));
-        friendListDesc.setBounds(100,100,200,50);
+        friendListDesc.setBounds(100, 100, 200, 50);
         friendListDesc.setHorizontalAlignment(SwingConstants.CENTER);
         add(friendListDesc);
 
@@ -72,8 +82,10 @@ public class ChatMain extends JFrame {
         JScrollPane friendListScroll = new JScrollPane(friendList,
                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         friendListScroll.getVerticalScrollBar().setUnitIncrement(15);
-        friendListScroll.setBounds(50,150,300,250);
+        friendListScroll.setBounds(50, 150, 300, 250);
         add(friendListScroll);
+
+
 
         // 여기서 친구 리스트를 불러온 뒤 반복문을 통해 데이터를 넣음
         // friend 클래스에 친구 이름을 넣어서 리스트에 추가함
@@ -95,6 +107,36 @@ public class ChatMain extends JFrame {
 
         // 리스트 아래에 실시간 공공정보
 
+        // 공공 데이터
+        // 1시간 단위의 데이터로 현재 시간과 가까운 날씨 예보를 받아와서 온도만 출력
+        // 장소는 가천대학교를 가정하여 성남시 수정구 복정동의 위도 및 경도를 사용
+        JLabel dataLabel = new JLabel("가천대학교 향후 6시간 기온 정보");
+        dataLabel.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+        dataLabel.setBounds(100, 400, 200, 50);
+        dataLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(dataLabel);
+
+        JSONArray weatherData = getWeatherData();
+        String[] header = new String[6];
+        String[][] contents = new String[1][6];
+        for (int i = 0; i < weatherData.length(); i++) {
+            header[i] = weatherData.getJSONObject(i).getString("fcstTime");
+            header[i] = header[i].substring(0, 2) + ":" + header[i].substring(2, 4);
+            contents[0][i] = weatherData.getJSONObject(i).getString("fcstValue") + "°C";
+            //System.out.println(weatherData.getJSONObject(i));
+        }
+
+        JTable jTable = new JTable(contents, header);
+        // 데이터 내용 가운데 정렬
+        DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
+        dtcr.setHorizontalAlignment(SwingConstants.CENTER);
+        for (int i = 0; i < jTable.getColumnModel().getColumnCount(); i++) {
+            jTable.getColumnModel().getColumn(i).setCellRenderer(dtcr);
+        }
+        JScrollPane scrolledTable=new JScrollPane(jTable);	//스크롤 될 수 있도록 JScrollPane 적용
+        scrolledTable.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        scrolledTable.setBounds(50, 440, 300,60);
+        add(scrolledTable);
 
         // 유저 검색
         // 검색된 유저 리스트
@@ -102,21 +144,23 @@ public class ChatMain extends JFrame {
 
         JLabel userSearchDesc = new JLabel("유저검색");
         userSearchDesc.setFont(new Font("맑은 고딕", Font.BOLD, 12));
-        userSearchDesc.setBounds(425,25,50,30);
+        userSearchDesc.setBounds(425, 25, 50, 30);
         userSearchDesc.setHorizontalAlignment(SwingConstants.CENTER);
         add(userSearchDesc);
 
         JTextField userSearchField = new JTextField(20);
-        userSearchField.setBounds(500,25,250,30);
+        userSearchField.setBounds(500, 25, 250, 30);
         add(userSearchField);
 
         userSearchField.getDocument().addDocumentListener(new DocumentListener() {
             public void changedUpdate(DocumentEvent e) {
                 search();
             }
+
             public void removeUpdate(DocumentEvent e) {
                 search();
             }
+
             public void insertUpdate(DocumentEvent e) {
                 search();
             }
@@ -134,7 +178,7 @@ public class ChatMain extends JFrame {
         JScrollPane userSearchListScroll = new JScrollPane(userSearchList,
                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         userSearchListScroll.getVerticalScrollBar().setUnitIncrement(15);
-        userSearchListScroll.setBounds(425,100,325,400);
+        userSearchListScroll.setBounds(425, 100, 325, 400);
         add(userSearchListScroll);
 
         // 반복문 시작
@@ -147,12 +191,11 @@ public class ChatMain extends JFrame {
 
     static class friend extends JLabel {
 
-        friend(String friendName)
-        {
+        friend(String friendName) {
             super(friendName);
             setFont(new Font("맑은 고딕", Font.BOLD, 12));
             Container comp = this;
-            setBorder(new EmptyBorder(10,20,10,0));
+            setBorder(new EmptyBorder(10, 20, 10, 0));
 
             /*
              * 우클릭 메뉴
@@ -176,7 +219,7 @@ public class ChatMain extends JFrame {
 
             addMouseListener(new MouseAdapter() {
                 public void mouseClicked(MouseEvent e) {
-                    if(e.getButton()==MouseEvent.BUTTON3)
+                    if (e.getButton() == MouseEvent.BUTTON3)
                         pm1.show(comp, e.getX(), e.getY());
                 }
             });
@@ -186,11 +229,11 @@ public class ChatMain extends JFrame {
 
     static class searched extends JLabel {
 
-        searched(String userName){
+        searched(String userName) {
             super(userName);
             setFont(new Font("맑은 고딕", Font.BOLD, 12));
             Container comp = this;
-            setBorder(new EmptyBorder(20,20,20,0));
+            setBorder(new EmptyBorder(20, 20, 20, 0));
 
             /*
              * 우클릭 메뉴
@@ -214,7 +257,7 @@ public class ChatMain extends JFrame {
 
             addMouseListener(new MouseAdapter() {
                 public void mouseClicked(MouseEvent e) {
-                    if(e.getButton()==MouseEvent.BUTTON3)
+                    if (e.getButton() == MouseEvent.BUTTON3)
                         pm1.show(comp, e.getX(), e.getY());
                 }
             });
@@ -226,7 +269,7 @@ public class ChatMain extends JFrame {
 
         detailInfo(String Id) {
             super("유저 상세정보");
-            setSize(300,225);
+            setSize(300, 225);
             setResizable(false);
             setLocationRelativeTo(null);
             setLayout(null);
@@ -234,36 +277,99 @@ public class ChatMain extends JFrame {
 
             JLabel userIdLabel = new JLabel("아이디 : " + "ID");
             userIdLabel.setFont(new Font("맑은 고딕", Font.BOLD, 12));
-            userIdLabel.setBounds(50,25,200,30);
+            userIdLabel.setBounds(50, 25, 200, 30);
             userIdLabel.setHorizontalAlignment(SwingConstants.CENTER);
             add(userIdLabel);
 
             JLabel usernameLabel = new JLabel("이름 : " + "이름");
             usernameLabel.setFont(new Font("맑은 고딕", Font.BOLD, 12));
-            usernameLabel.setBounds(50,50,200,30);
+            usernameLabel.setBounds(50, 50, 200, 30);
             usernameLabel.setHorizontalAlignment(SwingConstants.CENTER);
             add(usernameLabel);
 
             JLabel userNicknameLabel = new JLabel("별명 : " + "별명");
             userNicknameLabel.setFont(new Font("맑은 고딕", Font.BOLD, 12));
-            userNicknameLabel.setBounds(50,75,200,30);
+            userNicknameLabel.setBounds(50, 75, 200, 30);
             userNicknameLabel.setHorizontalAlignment(SwingConstants.CENTER);
             add(userNicknameLabel);
 
             JLabel userOnOffStateLabel = new JLabel("온/오프라인");
             userOnOffStateLabel.setFont(new Font("맑은 고딕", Font.BOLD, 12));
-            userOnOffStateLabel.setBounds(50,100,200,30);
+            userOnOffStateLabel.setBounds(50, 100, 200, 30);
             userOnOffStateLabel.setHorizontalAlignment(SwingConstants.CENTER);
             add(userOnOffStateLabel);
 
             JLabel lastConTimeLabel = new JLabel("시간");
             lastConTimeLabel.setFont(new Font("맑은 고딕", Font.BOLD, 12));
-            lastConTimeLabel.setBounds(50,125,200,30);
+            lastConTimeLabel.setBounds(50, 125, 200, 30);
             lastConTimeLabel.setHorizontalAlignment(SwingConstants.CENTER);
             add(lastConTimeLabel);
 
             setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             setVisible(true);
         }
+    }
+
+    public static JSONArray getWeatherData() {
+
+        // 현재 시간으로부터 1시간을 빼고 30분 단위를 맞출 수 있도록 또 시간을 뺀 이후 api 파라미터에 맞게 가공
+
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        LocalTime now = LocalTime.now().minusHours(1);
+        now = now.minusMinutes(now.getMinute() % 30);
+        if (now.getHour() == 0) today = today.minusDays(1);
+        String searchDate = today.format(formatter);
+
+        formatter = DateTimeFormatter.ofPattern("HHmm");
+        String searchTime = now.format(formatter);
+
+
+        // 공공 데이터 포탈 api 예제 활용
+
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst"); /*URL*/
+        urlBuilder.append("?").append(URLEncoder.encode("serviceKey", StandardCharsets.UTF_8)).append("=fJvYA1F%2BhGU1THwsH8VBFcneVsHQ95VLp5Osk145um6KQ%2FfYqEeVD4CWpeRGi8ZrwIMN7AUI3Ndto22tiGkgxw%3D%3D"); /*Service Key*/
+        urlBuilder.append("&").append(URLEncoder.encode("pageNo", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode("1", StandardCharsets.UTF_8)); /*페이지번호*/
+        urlBuilder.append("&").append(URLEncoder.encode("numOfRows", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode("1000", StandardCharsets.UTF_8)); /*한 페이지 결과 수*/
+        urlBuilder.append("&").append(URLEncoder.encode("dataType", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode("JSON", StandardCharsets.UTF_8)); /*요청자료형식(XML/JSON) Default: XML*/
+        urlBuilder.append("&").append(URLEncoder.encode("base_date", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode(searchDate, StandardCharsets.UTF_8)); /*‘21년 6월 28일 발표*/
+        urlBuilder.append("&").append(URLEncoder.encode("base_time", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode(searchTime, StandardCharsets.UTF_8)); /*06시 발표(정시단위) */
+        urlBuilder.append("&").append(URLEncoder.encode("nx", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode("62", StandardCharsets.UTF_8)); /*예보지점의 X 좌표값*/
+        urlBuilder.append("&").append(URLEncoder.encode("ny", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode("124", StandardCharsets.UTF_8)); /*예보지점의 Y 좌표값*/
+        URL url;
+        JSONArray ret = new JSONArray();
+        try {
+            url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+            System.out.println("Response code: " + conn.getResponseCode());
+            BufferedReader rd;
+            if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+
+            JSONArray jsonArray = new JSONObject(sb.toString()).getJSONObject("response").getJSONObject("body").getJSONObject("items").getJSONArray("item");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject obj = jsonArray.getJSONObject(i);
+                if (Objects.equals(obj.getString("category"), "T1H")) {
+                    ret.put(obj);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
     }
 }
