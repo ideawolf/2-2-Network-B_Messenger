f2b720e159489c5c2fa5649005df0d6275fa754f
Mincho <dldmstjq99@gmail.com>
2022-12-13 02:00:35 +0900

    Information Edit Apply to Client

--- src/model/ClientUser.java
+++ src/model/ClientUser.java
@@ -55,7 +55,7 @@ public class ClientUser {
         }
     }
 
-    private void getUserInfo() {
+    public void getUserInfo() {
         try {
             Socket socket = new Socket("localhost", 35014);
 
@@ -121,7 +121,6 @@ public class ClientUser {
             jsonObject.put("nickname", nickname);
             jsonObject.put("statusMessage", statusMessage);
             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            //BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             out.write(jsonObject.toString());
             out.newLine();
